995b4b36a5c2307dac8619e54c2b54874b3d424d
Vikasagrwl <105685053+Vikasagrwl@users.noreply.github.com>
2023-02-23 11:21:17 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,42 @@
 import React from 'react'
+import { useState } from 'react';
 import '../styles/App.css';
 const App = () => {
-
+  const[count, setCount]= useState(1);
+  const[color, setColor]= useState("normal");
   
+  const increment=()=>{
+      setCount(count+1);
+      colorChange(count+1);
+  }
+  const decrement=()=>{
+    if(count>1)
+      setCount(count-1);
+
+    colorChange(count-1);
+  }
+
+  function colorChange(count){
+    if(count%3==0){
+      setColor("fizz");
+    }
+    else if(count%5==0){
+      setColor("buzz")
+    }
+    else if(count%15==0){
+      setColor("fizzbuzz")
+    }
+    else{
+      setColor("normal")
+    }
+  }
   return (
     <div id="main">
-
+      <button onClick={increment} >increment</button>
+        <div className={color} >
+            {count}
+        </div>
+        <button onClick={decrement}>decrement</button>
     </div>
   )
 }
